2f42d79afec68e71c6567f7068e1d49ad62ae36e
rolansy <ronalshoeygeorge@gmail.com>
2024-03-26 10:04:51 +0530

    Add SJF scheduling algorithms

--- sys_call/cpusched.c
+++ sys_call/cpusched.c
@@ -5,7 +5,27 @@ typedef struct proc {
     int pid;
     int at;
     int bt;  
+    int wt;
+    int tat;
+    int ct;
+    int rt;
+    int prio;
 } proc;
+
+
+void disptable(proc* procs,int n){
+	printf("procs\tArrival Time\tBurst Time\tWaiting time\tTurnaround Time\tCompletion Time\tResponse Time\n");
+	int totwt=0,tottat=0;
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", procs[i].pid, procs[i].at, procs[i].bt, procs[i].wt, procs[i].tat, procs[i].ct, procs[i].rt);
+        totwt+=procs[i].wt;
+        tottat+=procs[i].tat;
+    }
+
+    printf("Average waiting time = %f", totwt / (float)n);
+    printf("\nAverage turn around time = %f \n", tottat /(float) n);
+
+}
 
 proc* createproc(int n) {
     proc* proces = (proc*)malloc(n * sizeof(proc));
@@ -37,55 +57,87 @@ void sortproc(proc* procs, int n) {
             }
         }
     }
-    printf("Sorted processes based on arrival time:\n");
+    printf("Sorted procs based on arrival time:\n");
     dispproc(procs, n);
 }
 
-void fcfs(proc* procs, int n) {
-    int wt[n], tat[n], ct[n], rt[n], totwt = 0, tottat = 0;
 
-    wt[0] = 0;
-    tat[0] = 0;
+
+void fcfs(proc* procs, int n) {
+    int totwt = 0, tottat = 0;
+
+    procs[0].wt = 0;
+    procs[0].tat = 0;
 	int time=0;
-	rt[0]=0;
+	procs[0].rt=0;
     for (int i = 0; i < n; i++) {
     	if( i==0){
-    		wt[i]=0;
-    		rt[i]=0;
+    		procs[i].wt=0;
+    		procs[i].rt=0;
     	}
     	else{
-    		wt[i]=time-procs[i].at;
-    		rt[i]=wt[i]-procs[i].at;
+    		procs[i].wt=time-procs[i].at;
+    		procs[i].rt=procs[i].wt-procs[i].at;
     	}
     	time+=procs[i].bt;
-    	tat[i]=procs[i].bt+wt[i];
-    	ct[i] = tat[i] + procs[i].at;
-    	printf("\n %d %d \n",time,ct[i]);
-    	tottat+=tat[i];
-    	totwt+=wt[i];
+    	procs[i].tat=procs[i].bt+procs[i].wt;
+    	procs[i].ct = procs[i].tat + procs[i].at;
+    	printf("\n %d %d \n",time,procs[i].ct);
+    	tottat+=procs[i].tat;
+    	totwt+=procs[i].wt;
     	
     }
-
+	printf("\nFCFS Schelduling\n");
+	disptable(procs, n);
     
-    printf("Processes\tArrival Time\tBurst Time\tWaiting time\tTurnaround Time\tCompletion Time\tResponse Time\n");
+    
+}
 
-    for (int i = 0; i < n; i++) {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", procs[i].pid, procs[i].at, procs[i].bt, wt[i], tat[i], ct[i], rt[i]);
+
+void sjf(proc* procs, int n) {
+    
+    proc temp;
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i+1; j < n; j++) {
+            if(procs[i].bt > procs[j].bt) {
+                temp = procs[i];
+                procs[i] = procs[j];
+                procs[j] = temp;
+            }
+        }
     }
 
-    printf("Average waiting time = %f", totwt / (float)n);
-    printf("\nAverage turn around time = %f \n", tottat /(float) n);
+
+    int currentTime = 0;
+    for (int i = 0; i < n; i++) {
+        if (currentTime < procs[i].at) {
+            currentTime = procs[i].at;
+        }
+
+        procs[i].rt = currentTime - procs[i].at;
+        procs[i].tat = procs[i].rt + procs[i].bt;
+        procs[i].wt = procs[i].tat - procs[i].bt;
+
+        currentTime += procs[i].bt;
+    }
+
+    printf("\nSJF Scheduling:\n");
+    disptable(procs, n);
 }
+
+
+
+
 
 void main(){
 	int c,n;
-	while (c!=5){
-		printf("1. Input Process\n2. display\n3. Sort Processes\n4. FCFS\n5.Exit\n");
+	while (c!=10){
+		printf("1. Input Process\n2. display\n3. Sort procs\n4. FCFS\n5.SJF\n10.Exit\n");
 		printf("Enter Choice : ");
 		scanf("%d",&c);
 		switch(c){
 			case 1:
-				printf("Enter Number of processes : ");
+				printf("Enter Number of procs : ");
 				scanf("%d", &n);
 				proc* procs = createproc(n);
 				break;
@@ -99,6 +151,10 @@ void main(){
        	 	case 4:
 				fcfs(procs,n);
 				break;
+			case 5:
+				sjf(procs,n);
+				break;
+				
 
 				
 			
